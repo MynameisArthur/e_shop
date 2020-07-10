f3fc6f6b8a9b411d247118984c978543f37a0f83
Arthur <arthurd800@wp.pl>
2020-07-10 11:57:18 +0200

    created form input components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Homepage from '../pages/homepage/Homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from '../pages/shop/Shop.component';
 import Header from '../components/header/Header.component';
+import SignInSignUp from '../pages/sign-in-and-sign-up/SignInSignUp.component';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route path="/shop" component={ShopPage} />
+                <Route path="/signin" component={SignInSignUp} />
             </Switch>
         </div>
     );
---
+++ src/pages/sign-in-and-sign-up/SignInSignUp.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SignInSignUp.styles.scss';
+import SignIn from '../../components/sign-in/SignIn.component';
+
+const SignInSignUp = () => {
+    return (
+        <div className="sign-in-and-sign-up">
+            <SignIn />
+        </div>
+    );
+};
+
+export default SignInSignUp;
