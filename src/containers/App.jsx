c922cbd7e53f611c6e8ac74cf975745b8b7209c2
Arthur <arthurd800@wp.pl>
2020-07-10 09:33:36 +0200

    updating app with a shop page, collection preview component, collection item component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Homepage from '../pages/homepage/Homepage.component';
-import {Switch, Route, Link} from 'react-router-dom';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
-
-const TopicsPage = (props) => (
-    <div>
-        <h1>Topic page</h1>
-        <Link to={`/comp/${props.match.params.id}`}>
-            {props.match.params.id}
-        </Link>
-    </div>
-);
-
-const Comp = (props) => {
-    return (
-        <div>
-            <h1>This is {props.match.params.id.toUpperCase()} page</h1>
-        </div>
-    );
-};
+import {Switch, Route} from 'react-router-dom';
+import ShopPage from '../pages/shop/Shop.component';
 
 const App = () => {
     return (
         <div className="App">
             <Switch>
                 <Route exact path="/" component={Homepage} />
-                <Route path="/hats" component={HatsPage} />
-                <Route path="/topics/:id" component={TopicsPage} />
-                <Route path="/comp/:id" component={Comp} />
+                <Route path="/shop" component={ShopPage} />
             </Switch>
         </div>
     );
