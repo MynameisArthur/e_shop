3b7e99fcd515345abe29587ef7cca8753afd0c26
Arthur <arthurd800@wp.pl>
2020-07-10 10:12:11 +0200

    Added navigation

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,10 +3,12 @@ import './App.scss';
 import Homepage from '../pages/homepage/Homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from '../pages/shop/Shop.component';
+import Header from '../components/header/Header.component';
 
 const App = () => {
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route path="/shop" component={ShopPage} />
