726e4f46aa2ae20f0ec2570df61d4e099d586d89
Arthur <arthurd800@wp.pl>
2020-07-14 12:05:07 +0200

    added and implemented redux-persist to cache cart items

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './containers/App';
 import './index.scss';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
             <React.StrictMode>
-                <App />
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
             </React.StrictMode>
         </Router>
     </Provider>,
