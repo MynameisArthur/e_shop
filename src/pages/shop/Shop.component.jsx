731ed753a8ef34cb4b37fbe7b10ac7683f281744
Arthur <arthurd800@wp.pl>
2020-07-15 11:58:18 +0200

    moved directory into it's own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview.component.jsx';
-import {selectCollections} from '../../redux/shop/shop.selectors.js';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/Collection.component';
 
-const ShopPage = ({collections}) => {
+const ShopPage = ({match}) => {
+    console.log(match);
     return (
         <div className="shop-page">
-            {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
+            <Route
+                exact
+                path={`${match.path}`}
+                component={CollectionsOverview}
+            />
+            <Route
+                path={`${match.path}/:collectionId`}
+                component={CollectionPage}
+            />
         </div>
     );
 };
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
