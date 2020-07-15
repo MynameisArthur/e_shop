61a4eb6a34462e5f8b74a2fcb914f61459034b7c
Arthur <arthurd800@wp.pl>
2020-07-15 13:10:58 +0200

    added stripe integration

---
+++ src/components/stripe-button/StripeButton.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_AXMmHZtBiYuJ42ynYhNNMczP';
+    const onToken = (token) => {
+        console.log(token);
+        alert('Payment Successfull');
+    };
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="E Shop INC."
+            billingAddress
+            shippingAddress
+            image="https://sendeyo.com/up/d/f3eb2117da"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/Checkout.component.jsx
+++ src/pages/checkout/Checkout.component.jsx
@@ -7,6 +7,7 @@ import {
     selectCartItems,
     selectCartTotal,
 } from '../../redux/cart/cart.selectors';
+import StripeCheckoutButton from '../../components/stripe-button/StripeButton.component';
 
 const CheckoutPage = ({cartItems, total}) => {
     return (
@@ -34,6 +35,12 @@ const CheckoutPage = ({cartItems, total}) => {
             <div className="total">
                 <span>TOTAL: ${total}</span>
             </div>
+            <div className="test-warning">
+                *Please use the following test credit for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total} />
         </div>
     );
 };
