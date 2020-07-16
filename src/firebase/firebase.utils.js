0be854e71a62186e682bdf0056b43ec3cd20b1b8
Arthur <arthurd800@wp.pl>
2020-07-16 10:36:07 +0200

    moved SHOP_DATA to firestore using utility firestore function

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,6 +16,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
     const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
+
     if (!snapShot.exists) {
         const {displayName, email} = userAuth;
         const createdAt = new Date();
@@ -34,6 +35,20 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 };
 
 firebase.initializeApp(config);
+
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+    return await batch.commit();
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
